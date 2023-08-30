c7c92d845f1a893f7d49ad8e72fe604a48e8aec5
MaxKreke <max.kreke@rub.de>
2023-08-30 15:53:35 +0200

    imported assets and configured framerate

--- Assets/Scripts/Adult Part/Terminal.cs
+++ Assets/Scripts/Adult Part/Terminal.cs
@@ -27,4 +27,9 @@ public class Terminal : MonoBehaviour
             }
         }
     }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(0, 0, 100, 100), (1.0f / Time.smoothDeltaTime).ToString());
+    }
 }
