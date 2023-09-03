ea4ddaf96ba6bfd46edd11b9ecfcdfd204498ad2
RB-mh <58522346+RB-mh@users.noreply.github.com>
2023-09-03 19:09:17 +0200

    Tamagochi Update
    Some Finetunig for Turn Based and Enemy Prefabs

--- Assets/Scripts/Tamagochi Part/BattleSystem.cs
+++ Assets/Scripts/Tamagochi Part/BattleSystem.cs
@@ -9,6 +9,11 @@ public class BattleSystem : MonoBehaviour
 {
 
     public GameObject playerPrefab;
+
+
+    /// <summary>
+    /// Hier sind die friend Prefabs
+    /// </summary>
     public GameObject friendPrefab;
 
     public BattleState state;
@@ -20,6 +25,8 @@ public class BattleSystem : MonoBehaviour
     public BattleHUD friendHUD;
     public GameObject buttons;
 
+    string[] possibleStats = new string[]{"hygiene", "charisma", "joy", "tomfoolery", "will", "anger"};
+
     // Start is called before the first frame update
     void Start()
     {
@@ -30,22 +37,39 @@ public class BattleSystem : MonoBehaviour
 
     IEnumerator SetupBattle()
     {
+
+        //Instantiate alles 
         GameObject playerGO = Instantiate(playerPrefab);
         tama = playerGO.GetComponent<Tamagochi>();
+
+        /// 
+        /// Hier werden die friend Prefabs geladen
+        /// 
         GameObject friendGO = Instantiate(friendPrefab);
         friend = friendGO.GetComponent<Friendo>();        
 
-        dialogueText.text = "Du sieht " + friend.friendoName + ". Versuche dich anzufreunden!!";
+        dialogueText.text = "You approach " + friend.friendoName + ". Try and make a friend!!";
         friendHUD.SetHUD(friend);
 
-        yield return new WaitForSeconds(3f);
+        yield return new WaitUntil(WaitOrClick);
 
-        if(tama.fashion > 10)
+
+        //fashion check
+        if(tama.fashion > 5)
         {
-            dialogueText.text = "Durch hinterlässt einen guten ersten Eindruck. " + friend.friendoName + " ist leichter anzufreunden!";
-            bool isdead = friend.TakeDamage(35, -1);
+            dialogueText.text = "Your fashion makes a good first impression " + friend.friendoName + " likes that!";
+            bool isdead = friend.TakeDamage(20 + 2* (tama.fashion - 5), -1);
             friendHUD.SetFriendship(friend.currentFriendship, friend.maxFriendship);
-            yield return new WaitForSeconds(3f);
+            yield return new WaitUntil(WaitOrClick);
+        }
+
+        //empathy check
+        for (int i = 0; i < 3; i++)
+        {
+            Image test = buttons.transform.GetChild(i).GetComponent<Image>();
+            float saturation = Mathf.Max(tama.empathy - 5, 0.0f) * 0.1f;
+            float hue = (friend.modifiers[i] + 1) * 40 /360;
+            test.color = Color.HSVToRGB(hue, saturation, 1.0f);
         }
 
         state = BattleState.PLAYERTURN;
@@ -54,38 +78,48 @@ public class BattleSystem : MonoBehaviour
 
     void PlayerTurn()
     {
-        dialogueText.text = "Was wirst du tun?";
+        dialogueText.text = "What is your move?";
         buttons.SetActive(true);
     }
 
     IEnumerator PlayerAction(int type)
     {
+
+        //attacks
         buttons.SetActive(false);
         
         int stat = 0;
+        float mod = 10;
         switch (type)
         {
             case 0:
                 stat = tama.charisma;
-                dialogueText.text = "Du versuchst " + friend.friendoName + " zu schmeicheln";
+                dialogueText.text = "You try to compliment " + friend.friendoName;
+                mod = friend.modifiers[0];
                 break;
             case 1:
                 stat = tama.tomfoolery;
-                dialogueText.text = "Du machst einen Witz";
+                dialogueText.text = "You try to make a joke";
+                mod = friend.modifiers[1];
                 break;
             case 2:
                 stat = tama.anger;
-                dialogueText.text = "Du provozierst " + friend.friendoName ;
+                dialogueText.text = "You provoke " + friend.friendoName ;
+                mod = friend.modifiers[2];
                 break;
         }
         
         int damage = 15 + 2 * stat;
 
+        CheckEffectiveness(mod);
+
+
+        //Will check, crit chance
         int crit = Random.Range(0,15);
         if (tama.will > crit)
         {
             damage *= 2;
-            dialogueText.text += "\nDurch deinen starken Willen ist es sehr effektiv" ;
+            dialogueText.text += "\nYour will makes this move twice as effective!!!" ;
         }
         
 
@@ -100,66 +134,75 @@ public class BattleSystem : MonoBehaviour
         }
         
 
-        yield return new WaitForSeconds(3f);
+        yield return new WaitUntil(WaitOrClick);
 
-        int smelly = 0;
-        if (tama.hygiene < 5)
+        if (!isdead)
         {
-            smelly = (tama.hygiene - 5) *2;
-            dialogueText.text = "Du riechst! " + friend.friendoName + " mag das gar nicht.";
-        }
-        else if (tama.hygiene > 10)
-        {
-            smelly = (tama.hygiene -10) * 4;
-            dialogueText.text = "Du riechst .... gut! " + friend.friendoName + " mag das sehr.";
-        }
-        if (smelly != 0)
-        {
-            isdead = friend.TakeDamage(smelly, -1);
-            friendHUD.SetFriendship(friend.currentFriendship, friend.maxFriendship);
+            int smelly = 0;
+            if (tama.hygiene < 5)
+            {
+                smelly = (tama.hygiene - 5) *2;
+                dialogueText.text = "You smell! " + friend.friendoName + " does not like that.";
+            }
+            else if (tama.hygiene > 10)
+            {
+                smelly = (tama.hygiene -10) * 4;
+                dialogueText.text = "You smell .... good! " + friend.friendoName + " likes this alot.";
+            }
+            if (smelly != 0)
+            {
+                isdead = friend.TakeDamage(smelly, -1);
+                friendHUD.SetFriendship(friend.currentFriendship, friend.maxFriendship);
 
-            yield return new WaitForSeconds(3f);
-        }
+                yield return new WaitUntil(WaitOrClick);
+            }
 
 
 
-        if (isdead)
-        {
-            state = BattleState.WON;
-            EndBattle();
-        }
-        else
-        {
-            state = BattleState.ENEMYTURN;
-            StartCoroutine(EnemyTurn());
+            if (isdead)
+            {
+                state = BattleState.WON;
+                EndBattle();
+            }
+            else
+            {
+                state = BattleState.ENEMYTURN;
+                StartCoroutine(EnemyTurn());
+            }
         }
     }
 
 
+    // Hier endet das Battle und eine neue Szene beginnt
     void EndBattle()
     {
         if (state == BattleState.WON)
         {
-            dialogueText.text = "Du und " + friend.friendoName + " seid jetzt Freunde!";
+            dialogueText.text = "You and " + friend.friendoName + " are now friends! :)";
         }
         else if (state == BattleState.LOST)
         {
-            dialogueText.text = friend.friendoName + " hat die Schnauze voll von dir und geht nach Hause...";
+            dialogueText.text = friend.friendoName + " is fed up with you and goes home...  :()";
         }
     }
 
     IEnumerator EnemyTurn()
     {
-        dialogueText.text = friend.friendoName + " " + friend.attack + "!\nDein " + friend.reduceStat + "-Wert sinkt";
-        ReduceStat(friend.reduceStat);
+        string reduceStat = friend.reduceStat;
+        if (reduceStat == "random")
+        {
+            reduceStat = possibleStats[Random.Range(0,6)];
+        }
+        dialogueText.text = friend.friendoName + " " + friend.attack + "!\nYour " + reduceStat + " value fell! ";
+        ReduceStat(reduceStat);
 
-        yield return new WaitForSeconds(2f);
+        yield return new WaitUntil(WaitOrClick);
 
-        dialogueText.text = friend.friendoName + " verliert ein wenig die Geduld.";
+        dialogueText.text = friend.friendoName + " loses patience with you.";
         bool patienceGone = friend.LosePatience(20 - tama.joy);
         friendHUD.SetPatience(friend.currentPatience, friend.maxPatience);
 
-        yield return new WaitForSeconds(2f);
+        yield return new WaitUntil(WaitOrClick);
 
         if (patienceGone)
         {
@@ -179,15 +222,41 @@ public class BattleSystem : MonoBehaviour
     {
         switch (stat)
         {
-            case "Will":
+            case "will":
                 if (tama.will == 0)
-                {
-                    dialogueText.text += " ......... Aber der ist schon 0";
-                }
+                { dialogueText.text += " ......... But it's already at 0";}
                 else 
-                {
-                    tama.will -=1;
-                }
+                {tama.will -=1;}
+                break;
+            case "hygiene":
+                if (tama.hygiene == 0)
+                { dialogueText.text += " ......... But it's already at 0";}
+                else 
+                {tama.hygiene -=1;}
+                break;
+            case "charisma":
+                if (tama.charisma == 0)
+                { dialogueText.text += " ......... But it's already at 0";}
+                else 
+                {tama.charisma -=1;}
+                break;
+            case "joy":
+                if (tama.joy == 0)
+                { dialogueText.text += " ......... But it's already at 0";}
+                else 
+                {tama.joy -=1;}
+                break;
+            case "tomfoolery":
+                if (tama.tomfoolery == 0)
+                { dialogueText.text += " ......... But it's already at 0";}
+                else 
+                {tama.tomfoolery -=1;}
+                break;
+            case "anger":
+                if (tama.anger == 0)
+                { dialogueText.text += " ......... But it's already at 0";}
+                else 
+                {tama.fashion -=1;}
                 break;
             default:
                 break;
@@ -200,6 +269,42 @@ public class BattleSystem : MonoBehaviour
             return;
 
         StartCoroutine(PlayerAction(type));
+    }
+
+    bool WaitOrClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public void CheckEffectiveness(float mod)
+    {
+        if (mod < -1 || mod > 2)
+        {
+            return;
+        }
+        
+
+        if (mod >= 1.5)
+        {
+            dialogueText.text += ". " + friend.friendoName + " likes that a lot!!";
+        }
+        else if (mod <= -0.25f)
+        {
+            dialogueText.text += ". " + friend.friendoName + " hates that a lot!!";
+        }
+        else if (mod <= 0.5f && mod > -0.25f)
+        {
+            dialogueText.text += ". " + friend.friendoName + " does not like that.";
+        }
+        else 
+        {
+            dialogueText.text += ". " + friend.friendoName + " likes that.";
+        }
+
     }
 
 }
