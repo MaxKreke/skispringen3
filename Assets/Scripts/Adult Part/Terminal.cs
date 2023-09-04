d67da3721068a021791b41b10b659f0b2847bcd4
MaxKreke <max.kreke@rub.de>
2023-09-04 21:33:12 +0200

    commit

--- Assets/Scripts/Adult Part/Terminal.cs
+++ Assets/Scripts/Adult Part/Terminal.cs
@@ -96,7 +96,7 @@ public class Terminal : MonoBehaviour
 
     public void EndDay()
     {
-        //SceneManager.LoadScene("Battle Night");
+        SceneManager.LoadScene("Battle Night");
     }
 
 }
