9d9c74d526ba0fb4f353e18f475683edabd56d5f
MaxKreke <max.kreke@rub.de>
2023-09-04 18:51:37 +0200

    fixed tutorial

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Character")
+        {
+            SceneManager.LoadScene("Day");
+        }
+    }
+}
