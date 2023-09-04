2262ec3590efe722ddfc171119ec8a7b9313c7bb
MaxKreke <max.kreke@rub.de>
2023-09-04 21:03:58 +0200

    commit

--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
 
-    public int tier;
-    public Enemycontainer container;
+    private Enemycontainer container;
+    public GameObject[] enemies;
 
     // Start is called before the first frame update
     void Start()
@@ -22,13 +22,13 @@ public class Spawner : MonoBehaviour
 
     public void Spawn()
     {
-        GameObject enemy = Instantiate(container.enemy[getIndex()],transform.position, Quaternion.identity);
+        GameObject enemy = Instantiate(enemies[getIndex()],transform.position, Quaternion.identity);
         enemy.transform.parent = container.gameObject.transform;
 
     }
 
     public int getIndex()
     {
-        return tier+Terminal.Day-1;
+        return Terminal.Day-1;
     }
 }
